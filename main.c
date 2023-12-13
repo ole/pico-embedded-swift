421152d4172da928ec4d70cf100b82f25a9069e3
Ole Begemann <ole@oleb.net>
2023-12-13 21:21:24 +0100

    Tell Swift to use armv6m-none-none-eabi instruction set for RP2040 (armv6 was wrong)
    We also need to provide some more shims to make the linker happy.

--- main.c
+++ main.c
@@ -34,3 +34,32 @@ int posix_memalign(void **memptr, size_t alignment, size_t size)
     printf("posix_memalign called\n");
     return -1;
 }
+
+_Bool __atomic_compare_exchange_4(volatile void *, void *, unsigned int, _Bool, int, int)
+{
+    printf("__atomic_compare_exchange_4 called\n");
+    return false;
+};
+
+unsigned int __atomic_fetch_add_4(volatile void *, unsigned int, int)
+{
+    printf("__atomic_fetch_add_4 called\n");
+    return 0;
+}
+
+unsigned int __atomic_fetch_sub_4(volatile void *, unsigned int, int)
+{
+    printf("__atomic_fetch_sub_4 called\n");
+    return 0;
+}
+
+unsigned int __atomic_load_4(const volatile void *, int)
+{
+    printf("__atomic_load_4 called\n");
+    return 0;
+}
+
+void __atomic_store_4(volatile void *, unsigned int, int)
+{
+    printf("_store_4 called\n");
+};
